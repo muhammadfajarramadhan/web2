f700ca4797a9d4a0ffb21b7cd59e769cdfa3de0b
muhammadfajarramadhan <79896679+muhammadfajarramadhan@users.noreply.github.com>
2021-03-17 23:20:38 +0700

    Create Pert7_Latihan8.php

---
+++ Pert7_Latihan8.php
@@ -0,0 +1,12 @@
+<HTML>
+<HEAD>
+	<TITLE> Penggunaan Join </TITLE>
+</HEAD>
+<BODY>
+	<?php
+	$var = array('18', '11', '2010');
+	$tanggal = join("/", $var);
+	echo "$tanggal";
+	?>
+</BODY>
+</HTML>
