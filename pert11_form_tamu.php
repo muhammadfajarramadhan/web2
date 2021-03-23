09b59bbc12c4850bcd499810500d230b7940961c
muhammadfajarramadhan <79896679+muhammadfajarramadhan@users.noreply.github.com>
2021-03-23 22:36:28 +0700

    Create pert11_form_tamu.php

---
+++ pert11_form_tamu.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Buku Tamu</title>
+</head>
+<body>
+<pre>
+<h2>Form Buku Tamu </h2>
+<hr size = 1>
+<form name = "tamu" method = "post" action = "simpan_tamu.php">
+ID        : <input type = "text" name = "id"><br>
+Nama      : <input type = "text" name = "name"><br>
+E-Mail    : <input type = "text" name = "email"><br>
+Alamat    : <input type = "text" name = "alamat"><br>
+Kota      : <input type = "text" name = "kota"><br>
+Pesan     : <br><textarea name = "msg"></textarea><br>
+<td><input type = "submit" name = "submit" value = "Simpan"> <input type = "reset" name = "reset" value = "Reset"></td>
+</form>
+</body>
+</html>
